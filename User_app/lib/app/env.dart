afb95977f976ecf0cc038e32e5ee9f5a761377ca
Yousef <amintrad1967@gmail.com>
2024-01-09 16:20:18 +0300

    project

---
+++ User_app/lib/app/env.dart
@@ -0,0 +1,15 @@
+/*
+  Authors : initappz (Rahul Jograna)
+  Website : https://initappz.com/
+  App Name : Ultimate Salon Full App Flutter V2
+  This App Template Source code is licensed as per the
+  terms found in the Website https://initappz.com/license
+  Copyright and Good Faith Purchasers © 2023-present initappz.
+*/
+class Environments {
+  static const String appName = 'Handy man';
+  static const String companyName = 'handyman';
+  static const String googleMapsKey = 'your-google-maps-key';
+  static const String apiBaseURL = 'https://al-marhala.online/public/';
+  static const String websiteURL = 'https://al-marhala.online/';
+}
